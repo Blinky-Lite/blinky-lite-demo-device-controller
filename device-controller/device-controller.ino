fc33a69e7b019a63d1a75f4f650a9472b9912276
David McGinnis <dmcginnis427@gmail.com>
2019-03-18 08:38:45 +0100

    binary io

--- device-controller/device-controller.ino
+++ device-controller/device-controller.ino
@@ -1,58 +1,75 @@
-#include "device-controller.h"
-
 const int led13Pin = 13;
+const int ledPin = 3;
+const int photoDetPin = A0;
+const int photoLedPin = 6;
+const int ultraTrigPin = 22;
+const int ultraEchoPin = 23;
+const int ultraLedPin = 7;
+const int tempPin  = A2;
+const int interruptTestInPin = 10;
+const int interruptTestOutPin = 11;
+
 boolean led13 = false;
 unsigned long nowTime;
-boolean heartbeat = false;
 int heartbeatPeriod = 1000;
 unsigned long startTimeHeartBeat;
 
-const int ledPin = 3;
-int ledState = 1;
-int ledValue = 255;
 int ledValueToggle = 0;
-int ledPeriod = 100;
 unsigned long startTimeLed = 0;
 
-const int photoDetPin = A0;
-const int photoLedPin = 6;
-int photoDetValue = 0;
 boolean photoLedValue = false;
-int photoDetPeriod = 1000;
 float photoLedPeriod = 250;
 float photoLedPeriodMin = 50;
 float photoLedPeriodMax = 2000;
 float photoLedMaxBright = 0.70;
-unsigned long startTimePhotoDet = 0;
 unsigned long startTimePhotoLed = 0;
 
-const int ultraTrigPin = 22;
-const int ultraEchoPin = 23;
-const int ultraLedPin = 7;
 float ultraLedValue = 0;
 long ultraDuration = 0;
-int ultraDistance = 0;
-int ultraAlarmDistance = 10;
-int ultraPeriod = 1000;
-unsigned long startTimeUltra = 0;
 
-const int interruptTestInPin = 10;
-const int interruptTestOutPin = 11;
 volatile boolean interruptFlag = false;
 volatile int interruptCounter = 0;
 const int maxInterruptCounts = 500;
 
-const int tempPin  = A2;
 int tempPinValue = 0;
 float tempValue = 0.0;
-float tempAvg = 0.0;
-int tempPeriod = 1000;
-unsigned long startTimeTemp = 0;
+
+struct Readings
+{
+  int photoDetValue;
+  int ultraDistance;
+  int ultraAlarmDistance;
+  float tempAvg;
+  int ledState;
+  int ledValue;
+  int ledPeriod;
+};
+Readings readings;
+
+struct Settings
+{
+  int echoSettings;
+  int ledState;
+  int ledValue;
+  int ledPeriod;
+  int ultraAlarmDistance;
+};
+Settings settings;
+
+struct SettingsMask
+{
+  int echoSettings;
+  int ledState;
+  int ledValue;
+  int ledPeriod;
+  int ultraAlarmDistance;
+};
+SettingsMask settingsMask;
 
 void setup()
 {
-  setupCommunications(true, 115200);
-  //Serial.begin(9600);
+  Serial1.begin(115200);
+  Serial.begin(9600);
       
   pinMode(led13Pin, OUTPUT); 
   pinMode(ledPin, OUTPUT); 
@@ -69,49 +86,76 @@ void setup()
   nowTime = millis();
   startTimeHeartBeat = nowTime;
   startTimeLed = nowTime;
-  startTimePhotoDet = nowTime;
-  startTimeUltra = nowTime;
-  startTimeTemp = nowTime;
-  
-  if (ledState == 2) analogWrite(ledPin, ledValue);    
-  attachInterrupt(interruptTestInPin, interruptTestHandler, HIGH);
+  readings.tempAvg = 0.0;
+  readings.ledState = 1;
+  readings.ledValue = 255;
+  readings.ledPeriod = 100;
+  readings.ultraAlarmDistance = 10;
 
- }
+
+  settings.echoSettings = 0;
+  
+  if (readings.ledState == 2) analogWrite(ledPin, readings.ledValue);    
+  attachInterrupt(interruptTestInPin, interruptTestHandler, HIGH);
+}
 
 void loop()
 {
-  if (dataOnSerial())
+  while(Serial1.available() > 0)
   {
-    if (getInputTopic().equals("getLedState")) printMessage("ledState", intToString(ledState));
-    if (getInputTopic().equals("getLedValue")) printMessage("ledValue", intToString(ledValue));
-    if (getInputTopic().equals("getLedPeriod")) printMessage("ledPeriod", intToString(ledPeriod));
-    if (getInputTopic().equals("setLedState"))
-    {
-      ledState = stringToInt(getInputPayload());
-      if (ledState == 0) analogWrite(ledPin, 0);
-      if (ledState == 2) analogWrite(ledPin, ledValue);
-      printMessage("ledState", intToString(ledState));
-
-    }
-    if (getInputTopic().equals("setLedValue"))
-    {
-      ledValue = stringToInt(getInputPayload());
-      if (ledState == 2) analogWrite(ledPin, ledValue);
-      printMessage("ledValue", intToString(ledValue));
-    }
-    if (getInputTopic().equals("setLedPeriod")) ledPeriod = stringToInt(getInputPayload());
-    if (getInputTopic().equals("setUltraAlarmDistance")) ultraAlarmDistance = stringToInt(getInputPayload());
-    
+    Serial1.readBytes((uint8_t*) &settingsMask,sizeof(settingsMask));
+    Serial1.readBytes((uint8_t*) &settings, sizeof(settings));
   }
-  nowTime = millis();
-  if (ledState == 1)
+  if (settingsMask.echoSettings > 0)
   {
-    if ((int)(nowTime - startTimeLed) > ledPeriod)
+     settingsMask.echoSettings = 0;
+     Serial1.write((uint8_t*)&readings, sizeof(readings));
+  }
+  if (settingsMask.ledState > 0)
+  {
+    if((0 <= settings.ledState) && (settings.ledState <= 2))
+    {
+      readings.ledState = settings.ledState;
+      if (readings.ledState == 0) analogWrite(ledPin, 0);
+      if (readings.ledState == 2) analogWrite(ledPin, readings.ledValue);
+    }
+    settingsMask.ledState = 0;
+  }
+  if (settingsMask.ledValue > 0)
+  {
+    if((0 <= settings.ledValue) && (settings.ledValue <= 255))
+    {
+      readings.ledValue = settings.ledValue;
+      if (readings.ledState == 2) analogWrite(ledPin, readings.ledValue);
+    }
+    settingsMask.ledValue = 0;
+  }
+  if (settingsMask.ledPeriod > 0)
+  {
+    if((100 <= settings.ledPeriod) && (settings.ledPeriod <= 10000))
+    {
+      readings.ledPeriod = settings.ledPeriod;
+    }
+    settingsMask.ledPeriod = 0;
+  }
+  if (settingsMask.ledPeriod > 0)
+  {
+    if((1 <= settings.ultraAlarmDistance) && (settings.ultraAlarmDistance <= 1000))
+    {
+      readings.ultraAlarmDistance = settings.ultraAlarmDistance;
+    }
+    settingsMask.ultraAlarmDistance = 0;
+  }
+
+  nowTime = millis();
+  if (readings.ledState == 1)
+  {
+    if ((int)(nowTime - startTimeLed) > readings.ledPeriod)
     {
       startTimeLed = nowTime;
       if (ledValueToggle == 0)
       {
-        ledValueToggle = ledValue;
+        ledValueToggle = readings.ledValue;
       }
       else
       {
@@ -121,17 +165,6 @@ void loop()
     }
   }
   
-  if ((int)(nowTime - startTimePhotoDet) > photoDetPeriod)
-  {
-    startTimePhotoDet = nowTime;
-    photoDetValue = analogRead(photoDetPin);
-
-    photoLedPeriod = (photoLedPeriodMax - photoLedPeriodMin)  / (photoLedMaxBright * 1024);
-    photoLedPeriod = photoLedPeriod * (photoDetValue - (1- photoLedMaxBright) * 1024) + photoLedPeriodMin;
-    if (photoLedPeriod < photoLedPeriodMin) photoLedPeriod = photoLedPeriodMin;
-   
-    printMessage("photoDetValue", intToString(photoDetValue));
-  }
   if ((int)(nowTime - startTimePhotoLed) > (int) photoLedPeriod)
   {
     startTimePhotoLed = nowTime;
@@ -139,9 +172,25 @@ void loop()
     digitalWrite(photoLedPin, photoLedValue);
   }
 
-  if ((int)(nowTime - startTimeUltra) > ultraPeriod)
+  tempPinValue = analogRead(tempPin);
+  tempValue = 3.2 * (((float) tempPinValue) / 1024.0);
+  tempValue = (215.0 * tempValue / (3.2 - tempValue)) - 100.6;
+  tempValue = 2.70 * tempValue;
+  readings.tempAvg = readings.tempAvg + (tempValue - readings.tempAvg) / 1000;
+ 
+
+  if ((int)(nowTime - startTimeHeartBeat) > heartbeatPeriod)
   {
-    startTimeUltra = nowTime;
+    startTimeHeartBeat = nowTime;
+    led13 = !led13;
+    digitalWrite(led13Pin, led13);
+
+    readings.photoDetValue = analogRead(photoDetPin);
+    photoLedPeriod = (photoLedPeriodMax - photoLedPeriodMin)  / (photoLedMaxBright * 1024);
+    photoLedPeriod = photoLedPeriod * (readings.photoDetValue - (1- photoLedMaxBright) * 1024) + photoLedPeriodMin;
+    if (photoLedPeriod < photoLedPeriodMin) photoLedPeriod = photoLedPeriodMin;
+    readings.photoDetValue = (1024 - readings.photoDetValue) / 10;
+
     digitalWrite(ultraTrigPin, LOW);
     delayMicroseconds(2);
     // Sets the trigPin on HIGH state for 10 micro seconds
@@ -151,45 +200,23 @@ void loop()
     // Reads the echoPin, returns the sound wave travel time in microseconds
     ultraDuration = pulseIn(ultraEchoPin, HIGH);
     // Calculating the distance
-    ultraDistance= ultraDuration * 0.034/2;
+    readings.ultraDistance= ultraDuration * 0.034/2;
     ultraLedValue = LOW;
-    if (ultraDistance <= ultraAlarmDistance) ultraLedValue = HIGH;
+    if (readings.ultraDistance <= readings.ultraAlarmDistance) ultraLedValue = HIGH;
     digitalWrite(ultraLedPin, ultraLedValue);
-    printMessage("ultraDistance", intToString(ultraDistance));
-  }
-
-  tempPinValue = analogRead(tempPin);
-  tempValue = 3.2 * (((float) tempPinValue) / 1024.0);
-  tempValue = (215.0 * tempValue / (3.2 - tempValue)) - 100.6;
-  tempValue = 2.70 * tempValue;
-  tempAvg = tempAvg + (tempValue - tempAvg) / 1000;
 
 
-  if ((int)(nowTime - startTimeTemp) > tempPeriod)
-  {
-    startTimeTemp = nowTime;
-    printMessage("tempAvg", floatToString(tempAvg,2));
-  }
- 
-
-  if ((int)(nowTime - startTimeHeartBeat) > heartbeatPeriod)
-  {
-    startTimeHeartBeat = nowTime;
-    heartbeat = !heartbeat;
-    led13 = !led13;
-    digitalWrite(led13Pin, led13);
-    printMessage("heartbeat", booleanToString(heartbeat));
   }
   if(interruptFlag)
   {
     interruptFlag = false;
     interruptCounter = 0;
     digitalWrite(interruptTestOutPin, LOW);
-    ledState = ledState + 1;
-    if (ledState > 2) ledState = 0;
-    if (ledState == 0) analogWrite(ledPin, 0);
-    if (ledState == 2) analogWrite(ledPin, ledValue);
-    printMessage("ledState", intToString(ledState));
+    readings.ledState = readings.ledState + 1;
+    if (readings.ledState > 2) readings.ledState = 0;
+    if (readings.ledState == 0) analogWrite(ledPin, 0);
+    if (readings.ledState == 2) analogWrite(ledPin, readings.ledValue);
+//    printMessage("ledState", intToString(readings.ledState));
   }
 
 }
